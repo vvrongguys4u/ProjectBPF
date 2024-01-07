cdf4e7d9e1e9994fb0b8ebf384982723ac361d61
vvrongguys4u <farhan22ti@mahasiswa.pcr.ac.id>
2024-01-07 20:56:51 +0700

    Change file name because there are error

--- application/controllers/Classes.php
+++ application/controllers/Classes.php
@@ -2,7 +2,7 @@
 defined('BASEPATH') or exit('No direct script access allowed');
 // Don't forget include/define REST_Controller path
 
-class Class extends CI_Controller
+class Classes extends CI_Controller
 {
 
   public function __construct()
