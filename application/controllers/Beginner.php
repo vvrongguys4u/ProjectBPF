3c98ad087f9c8c1905052101c5b28f8d34e6fab4
fadhillans <fadhilla22ti@mahasiswa.pcr.ac.id>
2024-01-07 23:06:07 +0700

    1

---
+++ application/controllers/Beginner.php
@@ -0,0 +1,20 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+// Don't forget include/define REST_Controller path
+
+class Beginner extends CI_Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function index()
+    {
+        $this->load->view("layout/header");
+        $this->load->view("course/beginner/vw_beginner");
+        $this->load->view("layout/footer");
+    }
+
+}
