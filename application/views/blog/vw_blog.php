97b0926ce9722bc3bfc84166072c7a3e09fed1fb
Abdi Alamsyah <abdi22ti@mahasiswa.pcr.ac.id>
2024-01-07 20:11:27 +0700

    update blog

--- application/views/blog/vw_blog.php
+++ application/views/blog/vw_blog.php
@@ -1,22 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<title>Blog</title>
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="description" content="Lingua project">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" type="text/css" href="styles/bootstrap4/bootstrap.min.css">
-<link href="plugins/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-<link rel="stylesheet" type="text/css" href="plugins/OwlCarousel2-2.2.1/owl.carousel.css">
-<link rel="stylesheet" type="text/css" href="plugins/OwlCarousel2-2.2.1/owl.theme.default.css">
-<link rel="stylesheet" type="text/css" href="plugins/OwlCarousel2-2.2.1/animate.css">
-<link href="plugins/video-js/video-js.css" rel="stylesheet" type="text/css">
-<link rel="stylesheet" type="text/css" href="styles/blog.css">
-<link rel="stylesheet" type="text/css" href="styles/blog_responsive.css">
-</head>
 <body>
-
 <div class="super_container">
 	
 	<!-- Home -->
@@ -40,7 +22,7 @@
 				<div class="col-xl-2 col-lg-4 col-md-6 blog_category_col">
 					<a href="blog.html">
 						<div class="blog_category">
-							<div class="blog_category_image"><img src="images/blog_1.jpg" alt=""></div>
+							<div class="blog_category_image"><img src="assets/images/blog_1.jpg" alt=""></div>
 							<div class="blog_category_title">travel</div>
 						</div>
 					</a>
@@ -50,7 +32,7 @@
 				<div class="col-xl-2 col-lg-4 col-md-6 blog_category_col">
 					<a href="blog.html">
 						<div class="blog_category">
-							<div class="blog_category_image"><img src="images/blog_2.jpg" alt=""></div>
+							<div class="blog_category_image"><img src="assets/images/blog_2.jpg" alt=""></div>
 							<div class="blog_category_title">languages</div>
 						</div>
 					</a>
@@ -60,7 +42,7 @@
 				<div class="col-xl-2 col-lg-4 col-md-6 blog_category_col">
 					<a href="blog.html">
 						<div class="blog_category">
-							<div class="blog_category_image"><img src="images/blog_3.jpg" alt=""></div>
+							<div class="blog_category_image"><img src="assets/images/blog_3.jpg" alt=""></div>
 							<div class="blog_category_title">cultures</div>
 						</div>
 					</a>
@@ -70,7 +52,7 @@
 				<div class="col-xl-2 col-lg-4 col-md-6 blog_category_col">
 					<a href="blog.html">
 						<div class="blog_category">
-							<div class="blog_category_image"><img src="images/blog_4.jpg" alt=""></div>
+							<div class="blog_category_image"><img src="assets/images/blog_4.jpg" alt=""></div>
 							<div class="blog_category_title">fashion</div>
 						</div>
 					</a>
@@ -80,7 +62,7 @@
 				<div class="col-xl-2 col-lg-4 col-md-6 blog_category_col">
 					<a href="blog.html">
 						<div class="blog_category">
-							<div class="blog_category_image"><img src="images/blog_5.jpg" alt=""></div>
+							<div class="blog_category_image"><img src="assets/images/blog_5.jpg" alt=""></div>
 							<div class="blog_category_title">cooking</div>
 						</div>
 					</a>
@@ -90,7 +72,7 @@
 				<div class="col-xl-2 col-lg-4 col-md-6 blog_category_col">
 					<a href="blog.html">
 						<div class="blog_category">
-							<div class="blog_category_image"><img src="images/blog_6.jpg" alt=""></div>
+							<div class="blog_category_image"><img src="assets/images/blog_6.jpg" alt=""></div>
 							<div class="blog_category_title">hobbies</div>
 						</div>
 					</a>
@@ -104,14 +86,14 @@
 				<!-- Blog Post -->
 				<div class="col-lg-6">
 					<div class="blog_post">
-						<div class="blog_image" style="background-image:url(images/blog_7.jpg)"></div>
+						<div class="blog_image" style="background-image:url(assets/images/blog_7.jpg)"></div>
 						<div class="blog_title_container">
 							<div class="blog_post_category"><a href="#">travel</a></div>
 							<div class="blog_post_title"><a href="blog_single.html">Design Better Forms</a></div>
 							<div class="blog_post_text">
 								<p>Whether it is a signup flow, a multi-view stepper, or a monotonous data entry interface, forms are one of the most important components of digital product design.</p>
 							</div>
-							<div class="read_more"><a href="blog_single.html">Read More <img src="images/right.png" alt=""></a></div>
+							<div class="read_more"><a href="blog_single.html">Read More <img src="assets/images/right.png" alt=""></a></div>
 						</div>
 					</div>
 				</div>
@@ -119,14 +101,14 @@
 				<!-- Blog Post -->
 				<div class="col-lg-6">
 					<div class="blog_post">
-						<div class="blog_image" style="background-image:url(images/blog_8.jpg)"></div>
+						<div class="blog_image" style="background-image:url(assets/images/blog_8.jpg)"></div>
 						<div class="blog_title_container">
 							<div class="blog_post_category"><a href="#">travel</a></div>
 							<div class="blog_post_title"><a href="blog_single.html">Art Helps Healing</a></div>
 							<div class="blog_post_text">
 								<p>Whether it is a signup flow, a multi-view stepper, or a monotonous data entry interface, forms are one of the most important components of digital product design.</p>
 							</div>
-							<div class="read_more"><a href="blog_single.html">Read More <img src="images/right.png" alt=""></a></div>
+							<div class="read_more"><a href="blog_single.html">Read More <img src="assets/images/right.png" alt=""></a></div>
 						</div>
 					</div>
 				</div>
@@ -134,14 +116,14 @@
 				<!-- Blog Post -->
 				<div class="col-lg-6">
 					<div class="blog_post">
-						<div class="blog_image" style="background-image:url(images/blog_9.jpg)"></div>
+						<div class="blog_image" style="background-image:url(assets/images/blog_9.jpg)"></div>
 						<div class="blog_title_container">
 							<div class="blog_post_category"><a href="#">travel</a></div>
 							<div class="blog_post_title"><a href="blog_single.html">Art Helps Healing</a></div>
 							<div class="blog_post_text">
 								<p>Whether it is a signup flow, a multi-view stepper, or a monotonous data entry interface, forms are one of the most important components of digital product design.</p>
 							</div>
-							<div class="read_more"><a href="blog_single.html">Read More <img src="images/right.png" alt=""></a></div>
+							<div class="read_more"><a href="blog_single.html">Read More <img src="assets/images/right.png" alt=""></a></div>
 						</div>
 					</div>
 				</div>
@@ -149,14 +131,14 @@
 				<!-- Blog Post -->
 				<div class="col-lg-6">
 					<div class="blog_post">
-						<div class="blog_image" style="background-image:url(images/blog_10.jpg)"></div>
+						<div class="blog_image" style="background-image:url(assets/images/blog_10.jpg)"></div>
 						<div class="blog_title_container">
 							<div class="blog_post_category"><a href="#">travel</a></div>
 							<div class="blog_post_title"><a href="blog_single.html">Art Helps Healing</a></div>
 							<div class="blog_post_text">
 								<p>Whether it is a signup flow, a multi-view stepper, or a monotonous data entry interface, forms are one of the most important components of digital product design.</p>
 							</div>
-							<div class="read_more"><a href="blog_single.html">Read More <img src="images/right.png" alt=""></a></div>
+							<div class="read_more"><a href="blog_single.html">Read More <img src="assets/images/right.png" alt=""></a></div>
 						</div>
 					</div>
 				</div>
@@ -164,14 +146,14 @@
 				<!-- Blog Post -->
 				<div class="col-lg-6">
 					<div class="blog_post">
-						<div class="blog_image" style="background-image:url(images/blog_11.jpg)"></div>
+						<div class="blog_image" style="background-image:url(assets/images/blog_11.jpg)"></div>
 						<div class="blog_title_container">
 							<div class="blog_post_category"><a href="#">travel</a></div>
 							<div class="blog_post_title"><a href="blog_single.html">Art Helps Healing</a></div>
 							<div class="blog_post_text">
 								<p>Whether it is a signup flow, a multi-view stepper, or a monotonous data entry interface, forms are one of the most important components of digital product design.</p>
 							</div>
-							<div class="read_more"><a href="blog_single.html">Read More <img src="images/right.png" alt=""></a></div>
+							<div class="read_more"><a href="blog_single.html">Read More <img src="assets/images/right.png" alt=""></a></div>
 						</div>
 					</div>
 				</div>
@@ -179,14 +161,14 @@
 				<!-- Blog Post -->
 				<div class="col-lg-6">
 					<div class="blog_post">
-						<div class="blog_image" style="background-image:url(images/blog_12.jpg)"></div>
+						<div class="blog_image" style="background-image:url(assets/images/blog_12.jpg)"></div>
 						<div class="blog_title_container">
 							<div class="blog_post_category"><a href="#">travel</a></div>
 							<div class="blog_post_title"><a href="blog_single.html">Art Helps Healing</a></div>
 							<div class="blog_post_text">
 								<p>Whether it is a signup flow, a multi-view stepper, or a monotonous data entry interface, forms are one of the most important components of digital product design.</p>
 							</div>
-							<div class="read_more"><a href="blog_single.html">Read More <img src="images/right.png" alt=""></a></div>
+							<div class="read_more"><a href="blog_single.html">Read More <img src="assets/images/right.png" alt=""></a></div>
 						</div>
 					</div>
 				</div>
@@ -202,13 +184,4 @@
 		</div>
 	</div>
 </div>
-
-<script src="js/jquery-3.2.1.min.js"></script>
-<script src="styles/bootstrap4/popper.js"></script>
-<script src="styles/bootstrap4/bootstrap.min.js"></script>
-<script src="plugins/OwlCarousel2-2.2.1/owl.carousel.js"></script>
-<script src="plugins/easing/easing.js"></script>
-<script src="plugins/parallax-js-master/parallax.min.js"></script>
-<script src="js/blog.js"></script>
 </body>
-</html>
