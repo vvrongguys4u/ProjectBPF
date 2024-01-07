d5a3a30c81cdde29a4aebf497989e983a73424fd
zikraar <151479106+zikraar@users.noreply.github.com>
2024-01-07 14:57:04 +0700

    Update Course (DONE)

--- application/views/courses/vw_courses.php
+++ application/views/courses/vw_courses.php
@@ -18,7 +18,7 @@
 
 	<!-- Courses -->
 
-	<div class="courses">
+	<div class="courses-intermediate">
 		<div class="container">
 			<div class="row courses_row">
 
@@ -35,7 +35,7 @@
 								</ul>
 							</div>
 							<div class="course_text">
-								<p>Learn all about the simple present verb tense in this Ellii grammar video! This clear explanation will help you understand when and why to use simple present verbs. You will also learn how different subject pronouns and nouns affect the verb form. This video includes a practice exercise at the end. Happy studying!</p>
+								<p>Learn all about the simple present verb tense in this Ellii grammar video! This clear explanation will help you understand when and why to use simple present verbs. Happy studying!</p>
 							</div>
 						</div>
 						<div class="course_footer d-flex flex-row align-items-center justify-content-start">
@@ -62,7 +62,7 @@
 								</ul>
 							</div>
 							<div class="course_text">
-								<p>The simple past verb tense is very important to be able to speak and write well in English! This Ellii grammar video clearly explains when and why to use the simple past. You will also learn the difference between regular and irregular past verbs. This video includes a practice exercise at the end. Happy studying!</p>
+								<p>The simple past verb tense is very important to be able to speak and write well in English! This Ellii grammar video clearly explains when and why to use the simple past.</p>
 							</div>
 						</div>
 						<div class="course_footer d-flex flex-row align-items-center justify-content-start">
@@ -89,7 +89,7 @@
 								</ul>
 							</div>
 							<div class="course_text">
-								<p>Learn when to use the simple future verb tense with "will" in this Ellii grammar video! We will also teach you contracted forms that are very common when speaking in English. This video includes a practice exercise at the end. Happy studying!</p>
+								<p>Learn when to use the simple future verb tense with "will" in this Ellii grammar video! We will also teach you contracted forms that are very common when speaking in English.</p>
 							</div>
 						</div>
 						<div class="course_footer d-flex flex-row align-items-center justify-content-start">
@@ -108,7 +108,7 @@
 					<div class="course-intermediate">
 						<div class="course_image"><img src="assets/images/thumbnailGVT4.jpg" alt=""></div>
 						<div class="course_body">
-							<div class="course_title"><a href="course.html">The Super Easy Way to Learn Pronouns in English | Types of Pronouns | List of Pronouns with Examples</a></div>
+							<div class="course_title"><a href="course.html">The Super Easy Way to Learn Pronouns in English</a></div>
 							<div class="course_info">
 								<ul>
 									<li><a href="instructors.html">7ESL Learning English</a></li>
@@ -116,10 +116,7 @@
 								</ul>
 							</div>
 							<div class="course_text">
-								<p>What is a pronoun in English grammar? One of the nine parts of speech in the English language is the pronoun. Pronoun is a word that substitutes for a noun or noun phrase.
-
-									A pronoun is used instead of a noun or noun phrase in a sentence. A pronoun may take place of the name of a person, place or thing.
-									Pronoun examples: I, me, we, they, you, he, she, it, yours, himself, ourselves, its, my, that, this, those, us, who, whom...</p>
+								<p>What is a pronoun in English grammar? One of the nine parts of speech in the English language is the pronoun. Pronoun is a word that substitutes for a noun or noun phrase. Hope you enjoy and Happy Studying!</p>
 							</div>
 						</div>
 						<div class="course_footer d-flex flex-row align-items-center justify-content-start">
@@ -159,7 +156,12 @@
 						</div>
 					</div>
 				</div>
-			</div>
+				</div>
+				</div>
+
+		<div class="courses-beginner">
+			<div class="container">
+				<div class="row courses_row">
 
 				<!-- Course-Beginner 1 -->
 			<div class="col-lg-4 course_col">
@@ -187,8 +189,177 @@
 						</div>
 					</div>
 				</div>
+
+				<!-- Course-Beginner 2 -->
+			<div class="col-lg-4 course_col">
+					<div class="course-beginner">
+						<div class="course_image"><img src="assets/images/thumbnailBeginner2.jpg" alt=""></div>
+						<div class="course_body">
+							<div class="course_title"><a href="course.html">Learn English Numbers 1-100</a></div>
+							<div class="course_info">
+								<ul>
+									<li><a href="instructors.html">Shaw English Online</a></li>
+									<li><a href="#">English</a></li>
+								</ul>
+							</div>
+							<div class="course_text">
+								<p>Let's learn about English numbers.  This is the perfect video to learn about the numbers 1 to 100. Robin teaches in an easy for you to learn and understand.</p>
+							</div>
+						</div>
+						<div class="course_footer d-flex flex-row align-items-center justify-content-start">
+							<div class="course_students"><i class="fa fa-user" aria-hidden="true"></i><span>10</span>
+							</div>
+							<div class="course_rating ml-auto"><i class="fa fa-star"
+									aria-hidden="true"></i><span>4,5</span>
+							</div>
+							<div class="course_mark trans_200"><a href="#">Free</a></div>
+						</div>
+					</div>
+				</div>
+
+				<!-- Course-Beginner 3 -->
+			<div class="col-lg-4 course_col">
+					<div class="course-beginner">
+						<div class="course_image"><img src="assets/images/thumbnailBeginner3.jpg" alt=""></div>
+						<div class="course_body">
+							<div class="course_title"><a href="course.html">Beginner 2 English Course: Calendar Vocabulary - Days, Weeks, Months, Years, and Seasons</a></div>
+							<div class="course_info">
+								<ul>
+									<li><a href="instructors.html">Shaw English Online</a></li>
+									<li><a href="#">English</a></li>
+								</ul>
+							</div>
+							<div class="course_text">
+								<p>Learn basic calendar vocabulary. This is from the Beginner 2 English grammar series by Robin Shaw.</p>
+							</div>
+						</div>
+						<div class="course_footer d-flex flex-row align-items-center justify-content-start">
+							<div class="course_students"><i class="fa fa-user" aria-hidden="true"></i><span>10</span>
+							</div>
+							<div class="course_rating ml-auto"><i class="fa fa-star"
+									aria-hidden="true"></i><span>4,5</span>
+							</div>
+							<div class="course_mark trans_200"><a href="#">Free</a></div>
+						</div>
+					</div>
+				</div>
+
+				<!-- Course-Beginner 4 -->
+			<div class="col-lg-4 course_col">
+					<div class="course-beginner">
+						<div class="course_image"><img src="assets/images/thumbnailBeginner4.jpg" alt=""></div>
+						<div class="course_body">
+							<div class="course_title"><a href="course.html">Seasons in English - Vocabulary lesson - winter, spring, summer, autumn / fall</a></div>
+							<div class="course_info">
+								<ul>
+									<li><a href="instructors.html">Woodward English</a></li>
+									<li><a href="#">English</a></li>
+								</ul>
+							</div>
+							<div class="course_text">
+								<p>The seasons in English - ESOL vocabulary lesson
+
+									We look at the names of the seasons in English and learn typical vocabulary associated with each season. Each word appears with a cartoon or picture to help understand the meaning of it.</p>
+							</div>
+						</div>
+						<div class="course_footer d-flex flex-row align-items-center justify-content-start">
+							<div class="course_students"><i class="fa fa-user" aria-hidden="true"></i><span>10</span>
+							</div>
+							<div class="course_rating ml-auto"><i class="fa fa-star"
+									aria-hidden="true"></i><span>4,5</span>
+							</div>
+							<div class="course_mark trans_200"><a href="#">Free</a></div>
+						</div>
+					</div>
+				</div>
+			</div>
+			</div>
+			</div>
 				
-				
+		<div class="courses-beginner">
+			<div class="container">
+				<div class="row courses_row">
+
+				<!-- Course-Advance 1 -->
+				<div class="col-lg-4 course_col">
+					<div class="course-advance">
+						<div class="course_image"><img src="assets/images/thumbnailAdvance1.jpg" alt=""></div>
+						<div class="course_body">
+							<div class="course_title"><a href="course.html">Gerunds & Infinitives – English Grammar Lessons</a></div>
+							<div class="course_info">
+								<ul>
+									<li><a href="instructors.html">Ellii (formerly ESL Library)</a></li>
+									<li><a href="#">English</a></li>
+								</ul>
+							</div>
+							<div class="course_text">
+								<p>When do we use gerunds (-ing verb) and infinitives (to + verb) in English? Let us teach you these handy tips so you know which one to use! This video also includes practice exercise at the end so you can test your knowledge. Happy studying!</p>
+							</div>
+						</div>
+						<div class="course_footer d-flex flex-row align-items-center justify-content-start">
+							<div class="course_students"><i class="fa fa-user" aria-hidden="true"></i><span>10</span>
+							</div>
+							<div class="course_rating ml-auto"><i class="fa fa-star"
+									aria-hidden="true"></i><span>4,5</span>
+							</div>
+							<div class="course_mark course_free trans_200"><a href="#">Free</a></div>
+						</div>
+					</div>
+				</div>
+
+				<!-- Course-Advance 2 -->
+				<div class="col-lg-4 course_col">
+					<div class="course-advance">
+						<div class="course_image"><img src="assets/images/thumbnailAdvance2.jpg" alt=""></div>
+						<div class="course_body">
+							<div class="course_title"><a href="course.html">Learn English Tenses: PAST PERFECT</a></div>
+							<div class="course_info">
+								<ul>
+									<li><a href="instructors.html">Learn English with Rebecca</a></li>
+									<li><a href="#">English</a></li>
+								</ul>
+							</div>
+							<div class="course_text">
+								<p>You will learn: structure, usage, pronunciation, spelling, contractions, questions, short answers, past participles, and irregular verbs. We’ll go forward systematically and practice together, so you understand fully.  This class is part of the engVid program on ALL the English verb tenses, designed to take your English to a higher level for career growth or academic success on English exams like the IELTS or TOEFL. The next class in this series compares the past simple and past perfect tenses.</p>
+							</div>
+						</div>
+						<div class="course_footer d-flex flex-row align-items-center justify-content-start">
+							<div class="course_students"><i class="fa fa-user" aria-hidden="true"></i><span>10</span>
+							</div>
+							<div class="course_rating ml-auto"><i class="fa fa-star"
+									aria-hidden="true"></i><span>4,5</span>
+							</div>
+							<div class="course_mark course_free trans_200"><a href="#">Free</a></div>
+						</div>
+					</div>
+				</div>
+
+				<!-- Course-Advance 3 -->
+				<div class="col-lg-4 course_col">
+					<div class="course-advance">
+						<div class="course_image"><img src="assets/images/thumbnailAdvance3.jpg" alt=""></div>
+						<div class="course_body">
+							<div class="course_title"><a href="course.html">GLearn English Tenses: PRESENT PERFECT</a></div>
+							<div class="course_info">
+								<ul>
+									<li><a href="instructors.html">Learn English with Rebecca</a></li>
+									<li><a href="#">English</a></li>
+								</ul>
+							</div>
+							<div class="course_text">Yet this advanced verb tense can lead to success in a job interview or on your IELTS! In this complete English class, I’ll show you how to use this tense, when to use it, and what mistakes to avoid. You will move forward step-by-step, to master this tense by learning structure, usage, pronunciation, spelling, contractions, questions, short answers, past participles, regular verbs, and irregular verbs</p>
+							</div>
+						</div>
+						<div class="course_footer d-flex flex-row align-items-center justify-content-start">
+							<div class="course_students"><i class="fa fa-user" aria-hidden="true"></i><span>10</span>
+							</div>
+							<div class="course_rating ml-auto"><i class="fa fa-star"
+									aria-hidden="true"></i><span>4,5</span>
+							</div>
+							<div class="course_mark course_free trans_200"><a href="#">Free</a></div>
+						</div>
+					</div>
+				</div>
+
 			</div>
 		</div>
 	</div>	
