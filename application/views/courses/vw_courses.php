4beaf0c68d2b7d533e552045aafedb1aa32b5ff0
zikraar <151479106+zikraar@users.noreply.github.com>
2024-01-07 16:15:24 +0700

    UPDATE COURSE

--- application/views/courses/vw_courses.php
+++ application/views/courses/vw_courses.php
@@ -25,7 +25,11 @@
 				<!-- Course -->
 				<div class="col-lg-4 course_col">
 					<div class="course-intermediate">
-						<div class="course_image"><img src="assets/images/thumbnailGVT.jpg" alt=""></div>
+						<div class="course_image" style="position: relative;"><a href="https://youtu.be/nvVdIJ0las0?feature=shared"><img src="assets/images/thumbnailGVT.jpg" alt=""><div style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);">
+        					<img src="assets/images/play_big.png" alt="Play Video">
+   					 	</div>
+					</div>
+
 						<div class="course_body">
 							<div class="course_title"><a href="course.html">Simple Present – Grammar & Verb Tenses</a></div>
 							<div class="course_info">
@@ -52,7 +56,10 @@
 				<!-- Course2 -->
 				<div class="col-lg-4 course_col">
 					<div class="course-intermediate">
-						<div class="course_image"><img src="assets/images/thumbnailGVT2.jpg" alt=""></div>
+						<div class="course_image" style="position: relative;"><a href="https://youtu.be/MI3S3kdkofo?feature=shared"><img src="assets/images/thumbnailGVT2.jpg" alt=""><div style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);">
+        					<img src="assets/images/play_big.png" alt="Play Video">
+   					 	</div>
+					</div>
 						<div class="course_body">
 							<div class="course_title"><a href="course.html">Simple Past – Grammar & Verb Tenses</a></div>
 							<div class="course_info">
@@ -72,14 +79,17 @@
 									aria-hidden="true"></i><span>4,5</span>
 							</div>
 							<div class="course_mark course_free trans_200"><a href="#">Free</a></div>
-						</div>
-					</div>
-				</div>
+			</div>
+		</div>
+	</div>
 
 				<!-- Course3 -->
 				<div class="col-lg-4 course_col">
 					<div class="course-intermediate">
-						<div class="course_image"><img src="assets/images/thumbnailGVT3.jpg" alt=""></div>
+						<div class="course_image" style="position: relative;"><a href="https://youtu.be/n14zCZAvSjI?feature=shared"><img src="assets/images/thumbnailGVT3.jpg" alt=""><div style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);">
+        					<img src="assets/images/play_big.png" alt="Play Video">
+   					 	</div>
+					</div>
 						<div class="course_body">
 							<div class="course_title"><a href="course.html">Simple Future with Will – Grammar & Verb Tenses</a></div>
 							<div class="course_info">
@@ -104,9 +114,12 @@
 				</div>
 
 				<!-- Course4 -->
-			<div class="col-lg-4 course_col">
+				<div class="col-lg-4 course_col">
 					<div class="course-intermediate">
-						<div class="course_image"><img src="assets/images/thumbnailGVT4.jpg" alt=""></div>
+						<div class="course_image" style="position: relative;"><a href="https://youtu.be/h_GnSOIfWf4?si=4RxaiZyXDDE3Nbja"><img src="assets/images/thumbnailGVT4.jpg" alt=""><div style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);">
+        					<img src="assets/images/play_big.png" alt="Play Video">
+   					 	</div>
+					</div>
 						<div class="course_body">
 							<div class="course_title"><a href="course.html">The Super Easy Way to Learn Pronouns in English</a></div>
 							<div class="course_info">
@@ -131,9 +144,12 @@
 				</div>
 
 				<!-- Course-Intermediate 5 -->
-			<div class="col-lg-4 course_col">
+				<div class="col-lg-4 course_col">
 					<div class="course-intermediate">
-						<div class="course_image"><img src="assets/images/thumbnailGVT5.jpg" alt=""></div>
+						<div class="course_image" style="position: relative;"><a href="https://youtu.be/kq65VGkVwC8?si=IoV37DY6wAbMPFfg"><img src="assets/images/thumbnailGVT5.jpg" alt=""><div style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);">
+        					<img src="assets/images/play_big.png" alt="Play Video">
+   					 	</div>
+					</div>
 						<div class="course_body">
 							<div class="course_title"><a href="course.html">Prepositions of Place – English Grammar Lessons</a></div>
 							<div class="course_info">
@@ -155,18 +171,21 @@
 							<div class="course_mark trans_200"><a href="#">Free</a></div>
 						</div>
 					</div>
-				</div>
-				</div>
-				</div>
+			</div>
+		</div>
+	</div>
 
 		<div class="courses-beginner">
 			<div class="container">
 				<div class="row courses_row">
 
 				<!-- Course-Beginner 1 -->
-			<div class="col-lg-4 course_col">
+				<div class="col-lg-4 course_col">
 					<div class="course-beginner">
-						<div class="course_image"><img src="assets/images/thumbnailBeginner1.jpg" alt=""></div>
+						<div class="course_image" style="position: relative;"><a href="https://youtu.be/Y2VAOHGUh00?si=Z39dGy06qWJAsYmg"><img src="assets/images/thumbnailBeginner1.jpg" alt=""><div style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);">
+        					<img src="assets/images/play_big.png" alt="Play Video">
+   					 	</div>
+					</div>
 						<div class="course_body">
 							<div class="course_title"><a href="course.html">Basic English Vocabulary: COLORS</a></div>
 							<div class="course_info">
@@ -191,9 +210,12 @@
 				</div>
 
 				<!-- Course-Beginner 2 -->
-			<div class="col-lg-4 course_col">
+				<div class="col-lg-4 course_col">
 					<div class="course-beginner">
-						<div class="course_image"><img src="assets/images/thumbnailBeginner2.jpg" alt=""></div>
+						<div class="course_image" style="position: relative;"><a href="https://youtu.be/D4eJ5kg28nU?si=DTB9L4QfA8wtlyZ5"><img src="assets/images/thumbnailBeginner2.jpg" alt=""><div style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);">
+        					<img src="assets/images/play_big.png" alt="Play Video">
+   					 	</div>
+					</div>
 						<div class="course_body">
 							<div class="course_title"><a href="course.html">Learn English Numbers 1-100</a></div>
 							<div class="course_info">
@@ -218,9 +240,12 @@
 				</div>
 
 				<!-- Course-Beginner 3 -->
-			<div class="col-lg-4 course_col">
+				<div class="col-lg-4 course_col">
 					<div class="course-beginner">
-						<div class="course_image"><img src="assets/images/thumbnailBeginner3.jpg" alt=""></div>
+						<div class="course_image" style="position: relative;"><a href="https://youtu.be/To6NiejpCzk?si=r-8qtswtFpaiUeL8"><img src="assets/images/thumbnailBeginner3.jpg" alt=""><div style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);">
+        					<img src="assets/images/play_big.png" alt="Play Video">
+   					 	</div>
+					</div>
 						<div class="course_body">
 							<div class="course_title"><a href="course.html">Calendar Vocabulary - Days, Weeks, Months, Years</a></div>
 							<div class="course_info">
@@ -245,9 +270,12 @@
 				</div>
 
 				<!-- Course-Beginner 4 -->
-			<div class="col-lg-4 course_col">
+				<div class="col-lg-4 course_col">
 					<div class="course-beginner">
-						<div class="course_image"><img src="assets/images/thumbnailBeginner4.jpg" alt=""></div>
+						<div class="course_image" style="position: relative;"><a href="https://youtu.be/Rd3gQPtgW_0?si=JE2JV1s40bW1F1GS"><img src="assets/images/thumbnailBeginner4.jpg" alt=""><div style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);">
+        					<img src="assets/images/play_big.png" alt="Play Video">
+   					 	</div>
+					</div>
 						<div class="course_body">
 							<div class="course_title"><a href="course.html">Seasons in English - Vocabulary lesson - winter, spring, summer, autumn / fall</a></div>
 							<div class="course_info">
@@ -283,7 +311,10 @@
 				<!-- Course-Advance 1 -->
 				<div class="col-lg-4 course_col">
 					<div class="course-advance">
-						<div class="course_image"><img src="assets/images/thumbnailAdvance1.jpg" alt=""></div>
+						<div class="course_image" style="position: relative;"><a href="https://youtu.be/-s1gu725tA4?si=5T9Vhv5nXoM9640i"><img src="assets/images/thumbnailAdvance1.jpg" alt=""><div style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);">
+        					<img src="assets/images/play_big.png" alt="Play Video">
+   					 	</div>
+					</div>
 						<div class="course_body">
 							<div class="course_title"><a href="course.html">Gerunds & Infinitives – English Grammar Lessons</a></div>
 							<div class="course_info">
@@ -310,17 +341,20 @@
 				<!-- Course-Advance 2 -->
 				<div class="col-lg-4 course_col">
 					<div class="course-advance">
-						<div class="course_image"><img src="assets/images/thumbnailAdvance2.jpg" alt=""></div>
+						<div class="course_image" style="position: relative;"><a href="https://youtu.be/XLIea-a-wE0?si=EPgnToWdicm3Qahf"><img src="assets/images/thumbnailAdvance2.jpg" alt=""><div style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);">
+        					<img src="assets/images/play_big.png" alt="Play Video">
+   					 	</div>
+					</div>
 						<div class="course_body">
-							<div class="course_title"><a href="course.html">Learn English Tenses: PAST PERFECT</a></div>
+							<div class="course_title"><a href="course.html">Gerunds & Infinitives – English Grammar Lessons</a></div>
 							<div class="course_info">
 								<ul>
-									<li><a href="instructors.html">Learn English with Rebecca</a></li>
+									<li><a href="instructors.html">Ellii (formerly ESL Library)</a></li>
 									<li><a href="#">English</a></li>
 								</ul>
 							</div>
 							<div class="course_text">
-								<p>You will learn: structure, usage, pronunciation, spelling, contractions, questions, short answers, past participles, and irregular verbs. We’ll go forward systematically and practice together, so you understand fully.  This class is part of the engVid program on ALL the English verb tenses, designed to take your English to a higher level for career growth or academic success on English exams like the IELTS or TOEFL. The next class in this series compares the past simple and past perfect tenses.</p>
+								<p>When do we use gerunds (-ing verb) and infinitives (to + verb) in English? Let us teach you these handy tips so you know which one to use! This video also includes practice exercise at the end so you can test your knowledge. Happy studying!</p>
 							</div>
 						</div>
 						<div class="course_footer d-flex flex-row align-items-center justify-content-start">
@@ -337,16 +371,20 @@
 				<!-- Course-Advance 3 -->
 				<div class="col-lg-4 course_col">
 					<div class="course-advance">
-						<div class="course_image"><img src="assets/images/thumbnailAdvance3.jpg" alt=""></div>
+						<div class="course_image" style="position: relative;"><a href="https://youtu.be/vd0yESrQMs0?si=6OxIzxLGQMAYo70Y"><img src="assets/images/thumbnailAdvance3.jpg" alt=""><div style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);">
+        					<img src="assets/images/play_big.png" alt="Play Video">
+   					 	</div>
+					</div>
 						<div class="course_body">
-							<div class="course_title"><a href="course.html">GLearn English Tenses: PRESENT PERFECT</a></div>
+							<div class="course_title"><a href="course.html">Gerunds & Infinitives – English Grammar Lessons</a></div>
 							<div class="course_info">
 								<ul>
-									<li><a href="instructors.html">Learn English with Rebecca</a></li>
+									<li><a href="instructors.html">Ellii (formerly ESL Library)</a></li>
 									<li><a href="#">English</a></li>
 								</ul>
 							</div>
-							<div class="course_text">Yet this advanced verb tense can lead to success in a job interview or on your IELTS! In this complete English class, I’ll show you how to use this tense, when to use it, and what mistakes to avoid. You will move forward step-by-step, to master this tense by learning structure, usage, pronunciation, spelling, contractions, questions, short answers, past participles, regular verbs, and irregular verbs</p>
+							<div class="course_text">
+								<p>When do we use gerunds (-ing verb) and infinitives (to + verb) in English? Let us teach you these handy tips so you know which one to use! This video also includes practice exercise at the end so you can test your knowledge. Happy studying!</p>
 							</div>
 						</div>
 						<div class="course_footer d-flex flex-row align-items-center justify-content-start">
@@ -360,6 +398,7 @@
 					</div>
 				</div>
 
+				
 			</div>
 		</div>
 	</div>	
